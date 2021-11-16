019b79fe388096663174aafa298f32fee5f367bb
Norberto <norgit@protonmail.com>
2021-11-16 12:51:21 -0300

    [create: dirs|files] make DOM manipulation exercise

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ejercicios S-04 DOM03
+
+### Ejercicio N°1
+Crear un boton que al ser clickeado muestre/oculte un elemento (toggle)
+
+### Ejercicio N°2
+Crear un `<ul>`
+Crear un `<input>`
+Crear un `<li>` con el valor del `<input>` (como si fueran notas que vas agregando)
+
+### Ejercicio N°3
+Usando new Date() insertar en el html el año actual junto a un saludo
+
+### Ejercicio N°4
+Crear un contenedor de 400x400 con 2 botones.
+Al ser clickeado el contenedor debe cambiar el background a rojo
+Cada boton debe estar dentro del contenedor y al clickear dar un `console.log('boton 1/2 se ejecuto')`
+
+### Ejercicio N°5
+Crear un form completamente desde javascript para insertar en nuestro html
---
+++ js/main.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const toTop = document.querySelector('#toTop')
+
+	const contentToggle = document.querySelector('#contenido')
+	const btnToggle = document.querySelector('#btnToggle')
+
+	const inputAddTask = document.querySelector('#inputAddTask')
+	const btnAddTask = document.querySelector('#btnAddTask')
+	const tasks = document.querySelector('#tasks')
+
+	const greetingContainer = document.querySelector('#saludo')
+
+	const changeContainer = document.querySelector('#changeContainer')
+	const btnChange1 = document.querySelector('#btnChange1')
+	const btnChangeReset = document.querySelector('#btnChangeReset')
+
+	const formContainer = document.querySelector('#formContainer')
+
+	btnToggle.addEventListener('click', () => contentToggle.classList.toggle('d-none'))
+
+	btnAddTask.addEventListener('click', () => {
+		const value = inputAddTask.value
+		const li = document.createElement('LI')
+		if (value.trim() === '') return alert('Ingresá una tarea válida')
+		li.innerText = value
+		tasks.appendChild(li)
+		inputAddTask.value = ''
+	})
+
+	greetingContainer.innerHTML = `<h3>Hola. Saludos en este año ${new Date().getFullYear()}</h3>`
+
+	btnChange1.addEventListener('click', () => changeContainer.style.backgroundColor = 'rgba(200, 50, 50, 1)')
+	btnChangeReset.addEventListener('click', () => changeContainer.style.backgroundColor = 'rgba(255, 255, 255, 0.2)')
+
+	formContainer.innerHTML = `
+		<form id="formUserData">
+			<input class="input" type="text" placeholder="Nombre" id="nombre">
+			<input class="input" type="text" placeholder="Apellido" id="apellido">
+			<input class="input" type="email" placeholder="Email" id="email">
+			<br>
+			<button class="btn" type="submit" id="btnSubmitData">Enviar datos</button>
+		</form>
+	`
+	document.querySelector('#formUserData').addEventListener('submit', e => e.preventDefault())
+
+	window.addEventListener('scroll', controlScroll)
+
+	toTop.addEventListener('click', () => {
+		document.body.scrollTop = 0
+		document.documentElement.scrollTop = 0
+	})
+
+	function controlScroll() {
+		const st = document.body.scrollTop || document.documentElement.scrollTop
+		const display = toTop.style.display
+		const cl = toTop.classList
+		if (st > 300) {
+			toTop.style.display = 'block'
+			cl.remove('hide')
+			cl.add('show')
+			setTimeout(() => {cl.add('rotate')}, 2100)
+	  } else {
+			if (display === 'block') {
+				cl.remove('show')
+				cl.add('hide')
+				setTimeout(() => {
+					cl.remove('rotate')
+					toTop.style.display = 'none'
+				}, 2000)
+			}
+	  }
+	}
+
+})
